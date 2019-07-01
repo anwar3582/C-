ec4aca5faf8e6957873e7882a36c69bb40084599
anwar3582 <anwar_carlos@hotmail.com>
2019-06-30 23:49:25 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 
-namespace C_
+namespace EjercicioExamen
 {
-    class Program
+    class Programd
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("una Palabra");
+           String palabra=Console.ReadLine();
+           char[] charPalabra = palabra.ToCharArray();
+            int inc=0;
+            int des = charPalabra.Length;
+
+            bool bError= false;
+
+            while((inc<des)&&(!bError)){
+                
+                if(charPalabra[inc]==charPalabra[des]){
+                    inc++;
+                    des--;
+                } else {
+                    bError=true;
+                }
+
+
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
